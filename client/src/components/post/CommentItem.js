3ed3d87ad1222cd1e0a991b02a601aa74c2dd82e
GowthamBalasubramaniyam <gowthambalasubramaniyam1@gmail.com>
2025-08-10 15:49:41 +0530

    Prepare for Railway deployment

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -8,48 +8,51 @@ import { deleteComment } from "../../actions/post";
 const CommentItem = ({
   postId,
   comment: { _id, text, name, avatar, user, date },
-    auth,
-    deleteComment
-}) => 
-    <div class="post bg-white p-1 my-1">
-      <div>
-        <Link to={`/profile/${user}`}>
-          <img
-            class="round-img"
-            src={avatar}
-            alt=""
-          />
-          <h4>{name}</h4>
-        </Link>
-      </div>
-      <div>
-        <p class="my-1">
-            {text}
-        </p>
-        <p class="post-date">Posted on 
-            {format(new Date(date), "MMMM dd, yyyy")}
-        </p>
-        {!auth.loading && user === auth.user._id && (
-          <button
-            onClick={(e) => deleteComment(postId, _id)}
-            type="button"
-            class="btn btn-danger"
-          >
-            <i class="fas fa-times"></i>
-          </button>
-        )}
-      </div>
+  auth,
+  deleteComment,
+}) => (
+  <div className="post bg-white p-1 my-1">
+    <div>
+      <Link to={`/profile/${user._id || user}`}>
+        <img
+          className="round-img"
+          src={
+            user.avatar ||
+            avatar ||
+            "//www.gravatar.com/avatar/9b473e5e0c6e0ae1e2a8708511284c66?s=200&r=pg&d=mm"
+          }
+          alt=""
+        />
+        <h4>{user.name || name}</h4>
+      </Link>
     </div>
- 
+    <div>
+      <p className="my-1">{text}</p>
+      <p className="post-date">
+        Posted on {format(new Date(date), "MMMM dd, yyyy")}
+      </p>
+      {!auth.loading && (user._id || user) === auth.user._id && (
+        <button
+          onClick={(e) => deleteComment(postId, _id)}
+          type="button"
+          className="btn btn-danger"
+        >
+          <i className="fas fa-times"></i>
+        </button>
+      )}
+    </div>
+  </div>
+);
+
 CommentItem.propTypes = {
-    postId: PropTypes.number.isRequired,
-    comment: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired,
-    deleteComment: PropTypes.func.isRequired
+  postId: PropTypes.string.isRequired, // Changed from number to string (MongoDB ObjectId)
+  comment: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+  deleteComment: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  auth: state.auth
+  auth: state.auth,
 });
 
-export default connect(mapStateToProps, {deleteComment})(CommentItem);
+export default connect(mapStateToProps, { deleteComment })(CommentItem);
