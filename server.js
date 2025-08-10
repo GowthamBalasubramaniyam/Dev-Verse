8797110533fe89cd22df421381451a00c3c94497
GowthamBalasubramaniyam <gowthambalasubramaniyam1@gmail.com>
2025-08-10 12:25:49 +0530

    Prepare for Railway deployment

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
 const express = require('express');
 const connectDb = require('./config/db');
-const userRoutes = require('./routes/api/users'); 
-const path = require('path'); 
-
+const path = require('path');
 
 const app = express();
 
-connectDb(); // Connect to the database
+// Connect to DB
+connectDb();
 
+// Middleware
+app.use(express.json());
 
-app.use(express.json({ extended: false })); // Middleware to parse JSON bodies
-
-
-app.use('/api/users', userRoutes); // Use the user routes defined in users.js
+// API routes
+app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-// Define the port and start the server
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
